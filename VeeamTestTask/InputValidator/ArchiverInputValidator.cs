e95254341877822fd7ee9ac404b61569d9053db6
Anastasiya Kameneva <kamenevaanast@mail.ru>
2020-02-24 15:34:59 +0500

    Refactor command line args parsing

--- VeeamTestTask/InputValidator/ArchiverInputValidator.cs
+++ VeeamTestTask/InputValidator/ArchiverInputValidator.cs
@@ -7,50 +7,8 @@ namespace VeeamTestTask
     /// <summary>
     /// Provides validation for console input arguments
     /// </summary>
-    public static class ArchiverInputValidator
+    public static class ArchiverFilePathValidator
     {
-        private const string CompressCommandString = "compress";
-        private const string DecompressCommandString = "decompress";
-        /// <summary>
-        /// Validates all string arguments <paramref name="args"/> using mask "compress/decompress [input file path] [output file path]"
-        /// </summary>
-        /// <param name="args">Arguments: command, input file path, output file path</param>
-        /// <returns></returns>
-        public static InputValidatorResult ValidateArgs(string[] args)
-        {
-            if (args.Length != 3)
-            {
-                return new InputValidatorResult(false, "Error: wrong input args. Example: compress/decompress [input file path] [output file path]");
-            }
-            var command = args[0];
-            var inputPath = args[1];
-            var outputPath = args[2];
-            if (!ValidateCommand(command))
-            {
-                return new InputValidatorResult(false, "Error: command was not recognized. Use key \"compress\" or \"decompress\".");
-
-            }
-            if (!ValidateInputPath(inputPath))
-            {
-                return new InputValidatorResult(false, "Error: input file does not exist. Try again.");
-
-            }
-            if (!ValidateOutputPath(outputPath))
-            {
-                return new InputValidatorResult(false, "Error: output file path is incorrect. Try again.");
-
-            }
-            return new InputValidatorResult(true);
-        }
-        /// <summary>
-        /// Validates command string
-        /// </summary>
-        /// <param name="command">Command string: compresss or decompress</param>
-        /// <returns></returns>
-        public static bool ValidateCommand(string command)
-        {
-            return command.ToLower().Equals(CompressCommandString) || command.ToLower().Equals(DecompressCommandString);
-        }
         /// <summary>
         /// Validates input file path
         /// </summary>
@@ -64,29 +22,12 @@ namespace VeeamTestTask
         /// Validates output file path
         /// </summary>
         /// <param name="outputPath">Output file path</param>
-        /// <returns> True if <paramref name="outputPath"/> was parsed as file path</returns>
+        /// <returns> True if <paramref name="outputPath"/> does not exists and does not contains any invalid file name characters</returns>
         public static bool ValidateOutputPath(string outputPath)
         {
-            string fileNamePattern = @"^(?!^(PRN|AUX|CLOCK\$|NUL|CON|COM\d|LPT\d|\..*)(\..+)?$)[^\x00-\x1f\\?*:\"";|/]+$";
-            return Regex.IsMatch(outputPath, fileNamePattern, RegexOptions.CultureInvariant);
-        }
-        /// <summary>
-        /// Checks if it is compress command or not
-        /// </summary>
-        /// <param name="command">Command string</param>
-        /// <returns>True if <paramref name="command"/> was recognizes as compress command; otherwise false</returns>
-        public static bool IsCompressCommand(string command)
-        {
-            return command.Equals(CompressCommandString);
-        }
-        /// <summary>
-        /// Checks if it is decompress command or not
-        /// </summary>
-        /// <param name="command">Command string</param>
-        /// <returns>True if <paramref name="command"/> was recognizes as decompress command; otherwise false</returns>
-        public static bool IsDecompressCommand(string command)
-        {
-            return command.Equals(DecompressCommandString);
+            return !string.IsNullOrEmpty(outputPath) &&
+              outputPath.IndexOfAny(Path.GetInvalidFileNameChars())<0
+              && !File.Exists(outputPath);
         }
     }
 }
--- VeeamTestTask/Program.cs
+++ VeeamTestTask/Program.cs
@@ -1,58 +1,158 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using CommandLine;
 
 namespace VeeamTestTask
 {
     class Program
     {
-        //TODO: 1. Parallel decompressing
-        //2. Reset progress if something goes wrong?
+        class Options
+        {
+            [Value(0, MetaName = "inputFilePath", HelpText = "Input file path")]
+            public string InputPath { get; set; }
+            [Value(1, MetaName = "outputFilePath", HelpText = "Output file path")]
+            public string OutputPath { get; set; }
+        }
+        [Verb("compress", HelpText = "Compress file")]
+        class CompressOptions: Options {}
+        [Verb("decompress", HelpText = "Decompress file")]
+        class DecompressOptions : Options { }
+
         static int Main(string[] args)
         {
-            //var validationResult = InputValidator.ValidateArgs(args);
-            //if (!validationResult.IsValid)
-            //{
-            //    Console.WriteLine(validationResult.ErrorMessage);
-            //    return 1;
-            //}
+            string[] testArgs = { "compress", "C:\\Test\\test-big.jpg", "C:\\Test\\testZip.mycomp" };
+            //string[] testArgs = { "decompress", "C:\\Test\\testZip.mycomp", "C:\\Test\\test-big1.jpg" };
+            return Parser.Default.ParseArguments<CompressOptions, DecompressOptions>(testArgs)
+                .MapResult(
+                  (CompressOptions opts) => Compress(opts),
+                  (DecompressOptions opts) => Decompress(opts),
+                  errs => 1);
+        }
 
-            //var command = args[0];
-            //var inputPath = args[1];
-            //var outputPath = args[2];
-
-            var command = "compress";
-            var inputPath = "C:\\Test\\test-big.iso";
-            var outputPath = "C:\\Test\\testZip.mycomp";
-            //command = "decompress";
-            //outputPath = "C:\\Test\\test-big1.jpg";
-            //inputPath = "C:\\Test\\testZip.mycomp";
-
+        private static int Decompress(DecompressOptions opts) { 
             IFileArchiver archiver = new FileArchiver();
             try
             {
-                Console.WriteLine("File {0} is processing, please wait...", inputPath);
-                if (ArchiverInputValidator.IsCompressCommand(command))
+                Console.WriteLine("File {0} is decompressing, please wait...", opts.InputPath);
+                if (ValidateFiles(opts))
                 {
-                    archiver.Compress(inputPath, outputPath);
+                    return 1;
                 }
-                if (ArchiverInputValidator.IsDecompressCommand(command))
-                {
-                    archiver.Decompress(inputPath, outputPath);
-                }
-                Console.WriteLine("File {0} was processed successfully. Result file located in {1}", inputPath, outputPath);
+                archiver.Decompress(opts.InputPath, opts.OutputPath);
+                Console.WriteLine("File {0} was processed successfully. Result file located in {1}", opts.InputPath, opts.OutputPath);
                 Console.ReadLine();
                 return 0;
             } catch(Exception ex)
             {
-                Console.WriteLine("Unexpected error during processing file: "+ex.Message);
+                Console.WriteLine("Unexpected error during decompressing file: " + ex.Message);
                 Console.WriteLine("Execution aborted");
                 Console.ReadLine();
                 return 1;
             }
+    }
+
+        private static int Compress(CompressOptions opts)
+        {
+            IFileArchiver archiver = new FileArchiver();
+            try
+            {
+                Console.WriteLine("File {0} is compressing, please wait...", opts.InputPath);
+                if (ValidateFiles(opts))
+                {
+                    return 1;
+                }
+                archiver.Compress(opts.InputPath, opts.OutputPath);
+                Console.WriteLine("File {0} was processed successfully. Result file located in {1}", opts.InputPath, opts.OutputPath);
+                Console.ReadLine();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected error during compressing file: " + ex.Message);
+                Console.WriteLine("Execution aborted");
+                Console.ReadLine();
+                return 1;
+            }
+        }
+        private static bool ValidateFiles(Options opts)
+        {
+            if (!ArchiverFilePathValidator.ValidateInputPath(opts.InputPath))
+            {
+                Console.WriteLine("Error: input file path is incorrect: file does not exists. Try again.");
+                return false;
+            }
+            if (!ArchiverFilePathValidator.ValidateOutputPath(opts.OutputPath))
+            {
+                Console.WriteLine("Error: output file path is incorrect or output file already exists. Try again.");
+                return false;
+            }
+            return true;
+        }
+        private int Test()
+        {
+            var inputFilePath = "C:\\Test\\test-big.dat";
+            var archivedFilePath = "C:\\Test\\testZip.mycomp";
+            var unarchivedFilePath = "C:\\Test\\test2.dat";
+
+            IFileArchiver archiver = new FileArchiver();
+            try
+            {
+                Console.WriteLine($"File {inputFilePath} is compressed, please wait...");
+                archiver.Compress(inputFilePath, archivedFilePath);
+
+                Console.WriteLine($"File {archivedFilePath} is decompressd, please wait...");
+                archiver.Decompress(archivedFilePath, unarchivedFilePath);
+
+                Console.WriteLine($"File {inputFilePath} was processed successfully. Result file located in {archivedFilePath} and {unarchivedFilePath}");
+
+                Console.WriteLine(CheсkEqualsTwoFiles(inputFilePath, unarchivedFilePath));
+                PrintMd5(inputFilePath);
+                PrintMd5(unarchivedFilePath);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected error during processing file: " + ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Execution aborted");
+
+                return 1;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        private static byte[] CalculateMD5(string path)
+        {
+            var hash = new byte[0];
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    hash = md5.ComputeHash(stream);
+                }
+            }
+            return hash;
+        }
+
+        private static string Md5toString(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        }
+
+        private static void PrintMd5(string path)
+        {
+            Console.WriteLine(Md5toString(CalculateMD5(path)));
+        }
+
+        private static bool CheсkEqualsTwoFiles(string firsFilePath, string secondFilePath)
+        {
+            var firstFileHash = CalculateMD5(firsFilePath);
+            var secondFileHash = CalculateMD5(secondFilePath);
+            return firstFileHash.SequenceEqual(secondFileHash);
         }
     }
 }
